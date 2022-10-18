ce2a68dd71d3b3687b263d714481406323d44260
INA <codechemi@gmail.com>
2022-10-18 15:32:03 +0900

    코드 리팩토링.

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Accordion from "react-bootstrap/Accordion";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
--- src/containers/introduceContainer.tsx
+++ src/containers/introduceContainer.tsx
@@ -4,16 +4,13 @@ import { useSelector, useDispatch } from "react-redux";
 // 루트 리듀서 가져오기
 import { RootState } from "../modules";
 // 액션 함수 가져오기
-import {
-  actionFuncArticle
-} from "../modules/introduce";
+import { actionFuncArticle } from "../modules/introduce";
 // 컴포넌트 가져오기
 import Introduce from "../components/introduce";
 let IntroduceContainer = () => {
   // 상태 조회
   const title = useSelector((state: RootState) => state.Introduce.title);
   const content = useSelector((state: RootState) => state.Introduce.content);
-
 
   return <Introduce title={title} content={content}></Introduce>;
 };
