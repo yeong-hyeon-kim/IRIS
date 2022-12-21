69fa77a5139e2da964eb191aed44f9862670dcae
INA <codechemi@gmail.com>
2022-12-21 17:53:16 +0900

    반응형 비율 조절

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,7 @@ function App() {
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   }
 
-  window.addEventListener("resize", () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  });
+  window.addEventListener('resize', () => setScreenSize());
 
   useEffect(() => {
     setScreenSize();
