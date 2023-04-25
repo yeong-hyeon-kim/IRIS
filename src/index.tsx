0114de0e48a21f88e240fe6d0747b83f8975d832
INA <codechemi@gmail.com>
2023-04-25 15:46:33 +0900

    refactor : Change CreateStore to ConfigStore

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 import rootReducer, { rootSaga } from "./modules/index";
-import createSagaMiddleware from "redux-saga";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "@redux-saga/core";
 
 import "bootstrap/dist/css/bootstrap.css";
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware))
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [sagaMiddleware],
+});
 
 sagaMiddleware.run(rootSaga);
 
